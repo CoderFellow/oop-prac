25a8f5a61235469f273975d327d3d15e490d80f8
Carlos Dising <disingcarlos7@gmail.com>
2025-05-02 08:47:09 +1000

    Add practice sets 1 and 2

---
+++ set 2/simple_class_with_init.py
@@ -0,0 +1,13 @@
+# creating class dog
+class car:
+	def __init__(self, brand, type):
+		self.brand=brand
+		self.type=type
+
+# creating objects of the car class
+my_car=car("Ford", "sportscar")
+dads_car=car("Toyota", "truck")
+
+# accessing the attributes of the object
+print(f"My car's brand is {my_car.brand}, and it's type is that of a {my_car.type}")
+print(f"My dad's car brand is {dads_car.brand}, and it's type is that of a {dads_car.type}")
